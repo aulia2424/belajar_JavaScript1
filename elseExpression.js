05fc68f8fc0fbf44f9b59d75aeb8f0d73a48f1bf
aulia2424 <auliamartha243@gmail.com>
2022-10-04 13:46:28 +0700

    modified file elseExpression.js

--- elseExpression.js
+++ elseExpression.js
@@ -4,28 +4,16 @@
 - kadang kita ingin melakukan eksekusi preogram tertentu jika konsidi if bernilai false
 - hal in bisa dilakukan menggunakan else expression
 */
-// const examValue = 90;
-// if (examValue < 50) {
-//   // => jika kurang dari 90 maka akan keluar else expression
-//   console.log("kerja bagus");
-// } else {
-//   console.log("maaf coba lagi");
-// }
+const examValue = 90;
+if (examValue < 50) {
+  // => jika kurang dari 90 maka akan keluar else expression
+  console.log("kerja bagus");
+} else {
+  console.log("maaf coba lagi");
+}
 
 // let stars = "";
 // for (let i = 0; i <= 5; i++) {
 //   stars += "*";
 //   console.log(stars);
 // }
-
-let stars = "";
-for (let i = 5; i >= 4; i++) {
-  stars += "*";
-  console.log(stars);
-}
-
-let starsDua = "";
-for (let i = 0; i <= 5; i++) {
-  starsDua += "*";
-  console.log(starsDua);
-}
--- elseIfExpression.js
+++ elseIfExpression.js
@@ -3,3 +3,14 @@
 - kadang dalam if, kita butuh membuat beberapa kondisi
 - kasus seperti ini, di js kita bisa menggunakan else is expression
 */
+const examValue = 90;
+if (examValue > 80) {
+  console.log("anda lulus nilai anda : A"); // jika nilai lebih besar dari 80 maka anda lulus
+} else if (examValue > 60) {
+  // jika nilai lebih besar dari 60 maka not bad
+  // else if berfungsi jika kita memiliki pilihan lebih dari dua atau memiliki dua pilihan
+  console.log("not bad nilai anda : B");
+} else {
+  // else sebagai penutup
+  console.log("maaf coba lagi nilai anda : C"); // jika di bawah 60 maka maaf coba lagi
+}
