b5b521028c42558a6fe00cdcda0dba1c7c1c6f80
aulia2424 <auliamartha243@gmail.com>
2022-10-04 13:13:57 +0700

    add file ifExpression.js

---
+++ ifExpression.js
@@ -0,0 +1,11 @@
+/*
+======IF EXPRESSION=====
+- dalam js, if adalah salah satu kata kunci yang digunakan untuk percabangan
+- percabangan artinya kita bisa mengeksekusi kode program tertentu ketika suatu kondisi terpenuhi
+- hampir disemua bahasa pemrogramman mendukung if expression
+*/
+const examValue = 90;
+if (examValue > 80) {
+  // => nilai disini harus true agar bisa di eksekusi, jika nilainya false maka tidak akan di eksekusi
+  console.log("kerja bagus");
+}
