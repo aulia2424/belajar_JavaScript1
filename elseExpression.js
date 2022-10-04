c566664795bebd30f6101bf1aa1321a5c4696091
aulia2424 <auliamartha243@gmail.com>
2022-10-04 13:26:09 +0700

    add file elseExpression.js

---
+++ elseExpression.js
@@ -0,0 +1,19 @@
+/*
+=====ELSE EXPRESION=====
+- blok is akan di eksekusi ketika kondisi if bernilai true
+- kadang kita ingin melakukan eksekusi preogram tertentu jika konsidi if bernilai false
+- hal in bisa dilakukan menggunakan else expression
+*/
+const examValue = 90;
+if (examValue < 50) {
+  // => jika kurang dari 90 maka akan keluar else expression
+  console.log("kerja bagus");
+} else {
+  console.log("maaf coba lagi");
+}
+
+// let stars = "";
+// for (let i = 0; i >= 5; i++) {
+//   stars += "*";
+//   console.log(stars);
+// }
