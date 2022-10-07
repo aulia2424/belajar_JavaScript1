74aa732456f6efb9041776786e94b59179c9f26c
aulia2424 <auliamartha243@gmail.com>
2022-10-07 10:08:01 +0700

    belajar Nullish Coalescing Operator

---
+++ nullishCoalescingOperator.js
@@ -0,0 +1,9 @@
+/* Nullish Coalescing Operator
+        - Nullish value adalah null dan undefined
+        - Nullish Coalescing Operator adalah operator mirip dengan ternary operator, yang membedakan adalah pada kondisi, jika bernilai null atau undefined, bertarti value defaultnya diambil.
+        -fungsi Nullish Coalescing Operator yaitu jika kita tidak memasukkan data ke properti kan biasanya hasilnya null/ undefined jika kita mamakai Nullish Coalescing Operator maka datanya bisa kita rubah/ditambahkan jadi bukan null/ undefined lagi
+      */
+console.log("-Nullish Coalescing Operator-");
+let parameter;
+data = parameter ?? "aul slebew";
+console.log(`${data}`);
