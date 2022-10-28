bf7780e68567ff1405ae070777792a9c3ac59cfd
aulia2424 <auliamartha243@gmail.com>
2022-10-28 20:15:31 +0700

    belajar typeof operator

--- typeOfOperator.js
+++ typeOfOperator.js
@@ -1,11 +1,10 @@
-<script>
-  /* Operator typeof
+/* Operator typeof
        - typeof merupakan operator yang bisa kita gunakan untuk melihat tipe data sebuah value atau variable.
        - Karena Js merupakan Dynamic language Artinya, Anda tak harus menuliskan tipe data saat mengisi variable seperti pada bahasa pemrograman lainnya
        - kadang kita perlu mengecek tipe data sebuah value / variable menggunakan operator typeof.
        -Hasil dari operator typeof adalah string.
       */
-  /*
+/*
        | Type      | Hasil Operator typeof |
        | Undefined | "undefined"           |
        | Null      | "object"              |
@@ -17,22 +16,20 @@
        | Function  | "function"            |
        | Lainnya   | "object"              |
       */
-  //artinya variable data misal adalah number nah apa yg akan di lakukan di alert
-  // jadi data di dalam alert adalah output yg akan kita keluarkan
-  // data keluar setelah NULL yang memiliki value sesuai dengan tipe datanya apa
-  document.writeln("-Operator typeof-");
-  document.writeln("<br>");
-  document.writeln("nanti keluar popup sesuai dengan tipe datanya");
-  let data = 10;
-  const typeData = typeof data;
-  if (typeof data === "number") {
-    alert("Number");
-  } else if (typeof data === "string") {
-    alert("String");
-  } else if (typeof data === "boolean") {
-    alert("Boolean");
-  } else if (typeof data === "object") {
-    alert("Object");
-  }
-  document.writeln(`${typeData}`);
-</script>
+//artinya variable data misal adalah number nah apa yg akan di lakukan di alert
+// jadi data di dalam alert adalah output yg akan kita keluarkan
+// data keluar setelah NULL yang memiliki value sesuai dengan tipe datanya apa
+// untuk mengecek tipedata
+console.log("-Operator typeof-");
+let data = 100;
+const typeData = typeof data;
+if (typeof data === "number") {
+  console.log("Number");
+} else if (typeof data === "string") {
+  console.log("String");
+} else if (typeof data === "boolean") {
+  console.log("Boolean");
+} else if (typeof data === "object") {
+  console.log("Object");
+}
+console.log(`${typeData}`);
